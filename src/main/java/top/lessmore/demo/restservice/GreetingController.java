f5f6ffb247bce15eb6a1b84d070ace312007c283
iron2column <iron.column.0717@gmail.com>
2022-11-16 19:56:05 +0800

    v3: Basic Query Parameter, but with @RequestParam

--- src/main/java/top/lessmore/demo/restservice/GreetingController.java
+++ src/main/java/top/lessmore/demo/restservice/GreetingController.java
@@ -1,6 +1,7 @@
 package top.lessmore.demo.restservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public Greeting greeting(String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(1, "Hello, " + name);
     }
 }
